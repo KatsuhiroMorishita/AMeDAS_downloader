6076177839329022e80472569672b7b05192c962
Katsuhiro Morishita <morimori.ynct@gmail.com>
2015-10-24 14:04:34 +0900

    download.pyの内部変数に_を付けたり改造
    アクセス制限を意識して内部変数に_を付けた。プロパティも追加した。

--- download.py
+++ download.py
@@ -16,7 +16,7 @@ from datetime import timedelta as td
 
 
 start_date = dt(2015,8,1) # 書き込んでいる日付けはテキトー
-end_date = dt(2015,8,2)
+end_date = dt(2015,8,24)
 
 
 def create_dir(path_list):
@@ -39,18 +39,18 @@ class amedas_node:
 	""" 個々のアメダス観測所に合わせた処理を実装したオブジェクト
 	"""
 	def __init__(self, prec_num, block_num, name, _id, area_code, group_code):
-		self.block_num = block_num
-		self.prec_num = prec_num
-		self.name = name
-		self.url_patr = None
+		self._block_num = block_num
+		self._prec_num = prec_num
+		self._name = name
+		self._url_patr = None
 		self._id = _id
-		self.area_code = area_code
-		self.group_code = group_code
+		self._area_code = area_code
+		self._group_code = group_code
 
-		if int(self.block_num) < 10000:
-			self.url_part = "a"
+		if int(self._block_num) < 10000:
+			self._url_part = "a"
 		else:
-			self.url_part = "s"
+			self._url_part = "s"
 
 	def get_data(self, _type="10min", date=None):
 		""" 指定されたデータタイプの観測データをダウンロードする
@@ -58,9 +58,9 @@ class amedas_node:
 		url = ""
 		if _type == "10min" or _type == "hourly":
 			url = "http://www.data.jma.go.jp/obd/stats/etrn/view/" + \
-				_type + "_" + self.url_part + "1.php?" + \
-				"prec_no=" + self.prec_num + \
-				"&block_no=" + self.block_num + \
+				_type + "_" + self._url_part + "1.php?" + \
+				"prec_no=" + self._prec_num + \
+				"&block_no=" + self._block_num + \
 				"&year=" + str(date.year) + "&month=" + date.strftime("%m") + "&day=" + date.strftime("%d") + "&view="
 		elif _type == "real-time":
 			url = "http://www.jma.go.jp/jp/amedas_h/today-" + self._id + ".html"
@@ -79,18 +79,22 @@ class amedas_node:
 		"""
 		html = self.get_data(_type, date)
 		if html != None:
-			_dir_path = ["Raw HTML", self.block_num + "_" + self.name, date.strftime("%Y")]
+			_dir_path = ["Raw HTML", self._block_num + "_" + self.name, date.strftime("%Y")]
 			_dir = create_dir(_dir_path)
-			fname = self.block_num + "_" + self.name + date.strftime("_%Y_%m_%d") + ".html"
+			fname = self._block_num + "_" + self.name + date.strftime("_%Y_%m_%d") + ".html"
 			path = os.path.join(_dir, fname)
 			with open(path, "wb") as fw:
 				fw.write(html)
+
+	@property
+	def name(self):
+	    return self._name
+	
 
 
 
 def get_amedas_nodes():
 	""" アメダス観測所の観測データへアクセスする辞書オブジェクトを返す
-	他のスクリプトから呼び出されても動作するのか？
 	"""
 	# 過去の観測データの入手に必要なアメダスのノード情報をファイルから読み取る
 	names1 = set() # 観測所名の一致チェック用
@@ -178,14 +182,13 @@ def main():
 		start_date = dt.now()
 		end_date = start_date
 
-	# 処理対象期間と観測所の情報を取得する
+	# 観測データをダウンロードして保存する
 	t = start_date
 	while t <= end_date:
 		for name in target:
 			node = amedas_nodes[name]
-			html = node.save(_type, t)
+			node.save(_type, t)
 			time.sleep(0.2)
-			print(html)
 		t += td(days=1)
 
 
